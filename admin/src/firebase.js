8b16daabe94f0f46bf910a27fcaae7690e19f09d
Enes <73299223+enescelep@users.noreply.github.com>
2021-09-05 16:07:20 +0300

    Firebase file edit

--- admin/src/firebase.js
+++ admin/src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCGkSoC1wFiiYxlUXnADNDExLRGvK6TFh4",
-  authDomain: "mern-netflix-e9b78.firebaseapp.com",
-  projectId: "mern-netflix-e9b78",
-  storageBucket: "mern-netflix-e9b78.appspot.com",
-  messagingSenderId: "557276442352",
-  appId: "1:557276442352:web:c2088d3ed909145c255c35",
-  measurementId: "G-MZ977XM5DV"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 firebase.initializeApp(firebaseConfig);
